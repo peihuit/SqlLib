fb6c8efc1b9821602bf60ec43b6127821001d095
peihuit <62690804+peihuit@users.noreply.github.com>
2020-11-17 14:43:44 +0800

    Add files via upload

---
+++ SqlLib/Models/RVal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlLib.Models
+{
+	public class RVal : Attribute
+	{
+		/// <summary>
+		/// 回傳狀態
+		/// </summary>
+		public bool RStatus { get; set; }
+		
+		/// <summary>
+		/// 回傳訊息
+		/// </summary>
+		public string RMsg { get; set; }
+
+		/// <summary>
+		/// 回傳的動態物件
+		/// </summary>
+		public dynamic DVal { get; set; }
+	}
+}
